f82f63a16e0ec042b99fe073a8c61716f807d4d7
Gurusharan Rajendra Gupta <gurusharanrajendra.gupta@cognizant.com>
2019-08-23 09:28:28 +0530

    Working code

--- src/main/java/com/cog/kafka/config/KafkaConsumerConfig.java
+++ src/main/java/com/cog/kafka/config/KafkaConsumerConfig.java
@@ -5,18 +5,14 @@ import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-
-import com.cog.kafka.model.Greetings;
 
 
 @EnableKafka
@@ -29,7 +25,7 @@ public class KafkaConsumerConfig {
 	@Value(value = "${kafka.consumer.group.id}")
 	private String groupId;
 	 
-    @Bean
+ /*   @Bean
     public ConsumerFactory<String, Greetings> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
         config.put(
@@ -55,5 +51,33 @@ public class KafkaConsumerConfig {
           = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
-    }
+    }*/
+	
+	   @Bean
+	    public ConsumerFactory<String, String> consumerFactory() {
+	        Map<String, Object> config = new HashMap<>();
+	        config.put(
+	          ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, 
+	          bootstrapAddress);
+	        config.put(
+	          ConsumerConfig.GROUP_ID_CONFIG, 
+	          groupId);
+	        config.put(
+	          ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, 
+	          StringDeserializer.class);
+	        config.put(
+	          ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, 
+	          StringDeserializer.class);
+	        return new DefaultKafkaConsumerFactory<>(config);
+	    }
+	 
+	    @Bean
+	    public ConcurrentKafkaListenerContainerFactory<String, String> 
+	      kafkaListenerContainerFactory() {
+	    
+	        ConcurrentKafkaListenerContainerFactory<String, String> factory
+	          = new ConcurrentKafkaListenerContainerFactory<>();
+	        factory.setConsumerFactory(consumerFactory());
+	        return factory;
+	    }
 }
--- src/main/java/com/cog/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/cog/kafka/consumer/KafkaConsumer.java
@@ -1,16 +1,21 @@
 package com.cog.kafka.consumer;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
-import com.cog.kafka.model.Greetings;
-
+@Service
 public class KafkaConsumer {
 
 	
 	@KafkaListener(topics = "${kafka.topic.name}", groupId = "${kafka.consumer.group.id}", containerFactory = "kafkaListenerContainerFactory")
-	public void listen(Greetings message) {
+	public void listen(String message) {
 	    System.out.println("Received Messasge in group foo: " + message.toString());
 	}
+	
+	/*@KafkaListener(topics = "${kafka.topic.name}", groupId = "${kafka.consumer.group.id}", containerFactory = "kafkaListenerContainerFactory")
+	public void listen(Greetings message) {
+	    System.out.println("Received Messasge in group foo: " + message.toString());
+	}*/
 	
 	/*@KafkaListener(topics = "topicName")
 	public void listenWithHeaders(
